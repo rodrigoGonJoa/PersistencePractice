aed2d2126a75bec068eb5787e0572ccd1086c0c8
Rodrigo González <69602297+rodrigoGonJoa@users.noreply.github.com>
2024-02-15 19:51:05 +0100

    Initial commit

---
+++ app/src/main/java/android/template/data/MyModelRepository.kt
@@ -0,0 +1,25 @@
+package android.template.data
+
+import android.template.data.local.database.MyModel
+import android.template.data.local.database.MyModelDao
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+interface MyModelRepository {
+    val myModels: Flow<List<String>>
+
+    suspend fun add(name: String)
+}
+
+class DefaultMyModelRepository @Inject constructor(
+    private val myModelDao: MyModelDao
+) : MyModelRepository {
+
+    override val myModels: Flow<List<String>> =
+        myModelDao.getMyModels().map { items -> items.map { it.name } }
+
+    override suspend fun add(name: String) {
+        myModelDao.insertMyModel(MyModel(name = name))
+    }
+}
---
+++ app/src/main/java/android/template/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package android.template.ui
+
+import android.os.Bundle
+import android.template.ui.theme.MyApplicationTheme
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainNavigation()
+                }
+            }
+        }
+    }
+}
